e55e28d2ddc0676d2450ec81760d5a7e6ebe316a
Andrey <a.stegno@gmail.com>
2016-03-30 11:56:34 +0000

    1.9.4

--- jquery.timepicker.js
+++ jquery.timepicker.js
@@ -3,7 +3,7 @@
  * Copyright (c) 2015 Jon Thornton - http://jonthornton.github.com/jquery-timepicker/
  * License: MIT
  */
-console.log("test")
+alert("test 1.9.2")
 
 (function (factory) {
     if (typeof exports === "object" && exports &&
